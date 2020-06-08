7d458871f77d3db9676db0b72aa45581892bde5c
devansh pala <59969598+devanshpala@users.noreply.github.com>
2020-06-08 19:01:14 +0530

    Add files via upload

--- jsFiles/main.js
+++ jsFiles/main.js
@@ -7,118 +7,162 @@ var app = new Vue({
         edits: ['Filters', 'Crop/Rotate', 'Flip','Resize'],
         editState: null,
         filterState: null,
+        rangeType: 0,
         onloadState: false,
         file: null,
         addedFilters: [],
         scale: 0,
         appendFilter: '',
-        filter: ['grayscale','invert','contrast','saturate','sepia'],
+        filter: ['grayscale','blur','opacity','hue-rotate','brightness','invert','contrast','saturate','sepia'],
         },
     methods: {
-        onSubmit() {
-            if(this.validate(this.input)) {
-                this.names.push(this.input);
-                this.input = '';
-                console.log('validated')
-            }
-            else {
-                alert('Please input Name')
-            }
+
+        getIndexOfScale(value) { // THIS METHOD CALLS TO FETCH THE PERCENTAGE
+            let indxOf = this.appendFilter.indexOf(value);
+            let len = value.length;
+            let carcode = this.appendFilter.charAt(indxOf+len+4);
+            let obj = {indxOf, len, carcode};
+            return obj;
         },
-        validate(value) {
-            if(value !== '')
-                return true;
-            else
-                return false;
-        },
-        findName() {
-             this.state = false;
-             let loosername = this.names[Math.floor(Math.random() * this.names.length)]
-             this.result = loosername;
-             console.log(loosername, this.result)
-        },
-        startAgain() {
-            this.names = []
-            this.state = true;
-        },
-        onChange(event) {
-            // console.log('onChange called')
+
+        onChange(event) { // THIS METHOD CALLS WHEN IMAGE IS LOADED
             let src = URL.createObjectURL(event.target.files[0]);
-            // console.log(src)
             this.file = src;
             this.onloadState = true;
         },
-        changeEditState(val) {
+
+        changeEditState(val) { // THIS METHOD CALLS WHENEVER
             this.editState = val;
         },
-        changeFilterState(val) {
-            // let val = this.filterState;
+
+        changeFilterState(val) { // THIS METHOD CALLS WHENEVER ANY FILTER IS CHANGED
+
             this.filterState = val;
-            // let
+            let tmp = this.filterState
+
+            // IF CONDITIONS FOR SETTING RANGETYPE
+            if(tmp === "brightness" || tmp === "contrast") {
+                this.rangeType = 2;
+
+            } else if( tmp === "invert" || tmp === "grayscale" || tmp === "sepia" || tmp === "opacity") {
+                this.rangeType = 1;
+
+            } else if(tmp === "saturate" || tmp === "hue-rotate") {
+                this.rangeType = 3;
+            } else {
+                this.rangeType = 4;
+            }
+
+            // FORLOOP FOR SETTING SCALE AT SPECIFIC position FOR SPECIFIC FILTER
             for(let item of this.addedFilters) {
-                if(val === item) {
-                    let idxOf = this.appendFilter.indexOf(val);
-                    let len = val.length;
-                    let carcode = this.appendFilter.charAt(idxOf+len+4);
-                    if(carcode == '%') {
-                            this.scale = 100
-                            console.log(this.scale,'100 percent');
-                            break;
+                if(this.filterState == item) {
+
+                    let lastChar = this.getIndexOfScale(this.filterState);
+                    let slc = 0;
+
+                    if(lastChar.carcode == '%' || lastChar.carcode == 'd' ||lastChar.carcode == 'p') {
+
+                        slc = this.appendFilter.slice(lastChar.indxOf+lastChar.len+1,
+                                                      lastChar.indxOf+lastChar.len+4);
+                        this.scale = parseInt(slc)
+                        break;
                     } else {
-                        let slc = 0
-                        slc = this.appendFilter.slice(idxOf+len+1,idxOf+len+3);
+                        slc = this.appendFilter.slice(lastChar.indxOf+lastChar.len+1,
+                                                      lastChar.indxOf+lastChar.len+3);
                         this.scale = parseInt(slc);
-                        console.log(this.scale);
                         break;
                     }
                 } else {
-                    this.scale = 0;
+
+                    if(this.rangeType == 1) {
+                        this.scale = 0;
+
+                    } else {
+                        this.scale = 100;
+
+                    }
                 }
-                console.log('final scale',this.scale)
+            }
+
+            //THIS CONDITION USED TO SET SCALE AT DEFAULT VALUE WHEN THERE ARE NO FILTERS APPENDED YET
+            if(this.addedFilters.length == 0) {
+                console.log('zero length added filter called')
+                if(this.rangeType == 1 || this.rangeType == 4) {
+                    this.scale = 0;
+                } else {
+                    this.scale = 100;
+                }
             }
 
         },
-        filterChange() {
+        filterChange() { // THIS METHOD CALLS WHENEVER SCALE OF THE FILTER CHANGES
             let isAdded = false;
             let filt = this.filterState
-            let result = ''+filt+'('+this.scale+'%)';
-            // console.log(filt);
+            let result =''
+            if(filt == "hue-rotate") {
+                result = ''+filt+'('+this.scale+'deg)';
+            }
+            else if(filt == "blur") {
+                result = ''+filt+'('+this.scale+'px)';
+            }
+            else {
+                result = ''+filt+'('+this.scale+'%)';
+            }
 
-            for(let item of this.addedFilters) {
+            console.log('filter change called')
+            // THIS LOOP CHECKS WHETHER ACTIVE FILTER IS ALREADY USED OR NOT
+            for(let item of this.addedFilters)  {
                 if(item == filt) {
                     isAdded = true;
                 }
             }
 
+            // IF THE FILTER IS NOT USED BEFORE, IT APPENDS TO ADDEDFILTERS ARRAY
             if(!isAdded) {
-
                 this.addedFilters.push(filt)
                 this.appendFilter += result;
-                // console.log('filter', this.addedFilters+' filter added');
                 document.getElementById('imge').style.filter = this.appendFilter;
+                console.log('addedfilter', this.addedFilters);
 
             } else {
 
-                let idxOf = this.appendFilter.indexOf(filt);
-                let len = filt.length;
-                let carcode = this.appendFilter.charAt(idxOf+len+4);
+                let lastchar = this.getIndexOfScale(filt);
                 let sstore = ''
+                if(lastchar.carcode == '%' || lastchar.carcode == 'd' || lastchar.carcode == 'p') {
+                        if(filt == 'blur') {
+                            console.log(lastchar.indxOf,lastchar.indxOf+lastchar.len+7)
+                            sstore = this.appendFilter.slice(lastchar.indxOf,
+                                                    lastchar.indxOf+lastchar.len+7);
+                        }
+                        else if(filt == 'hue-rotate') {
+                            sstore = this.appendFilter.slice(lastchar.indxOf,
+                                                    lastchar.indxOf+lastchar.len+8);
+                        } else {
+                            console.log('last else called',lastchar.indxOf,
+                                lastchar.indxOf+lastchar.len+6)
+                            sstore = this.appendFilter.slice(lastchar.indxOf,
+                                lastchar.indxOf+lastchar.len+6);
+                        }
 
-                if(carcode == '%') {
-                        console.log('% called')
-                        sstore = this.appendFilter.slice(idxOf,idxOf+len+6);
                 } else {
-                    console.log('% not called')
-                    // console.log('99 percent', idxOf, len+5)
-                        sstore = this.appendFilter.slice(idxOf,idxOf+len+5);
+                    if(filt == 'blur') {
+                        sstore = this.appendFilter.slice(lastchar.indxOf,
+                                                lastchar.indxOf+lastchar.len+6);
+                    }
+                    else if(filt == 'hue-rotate') {
+                        sstore = this.appendFilter.slice(lastchar.indxOf,
+                                                lastchar.indxOf+lastchar.len+7);
+                    } else {
+                        sstore = this.appendFilter.slice(lastchar.indxOf,
+                                                lastchar.indxOf+lastchar.len+5);
+                    }
+
                 }
-                console.log(idxOf, sstore, len+5)
+
                 this.appendFilter = this.appendFilter.replace(sstore,result)
-                // console.log('2nd time called', this.appendFilter)
                 document.getElementById('imge').style.filter = this.appendFilter;
             }
-            console.log(this.appendFilter);
-            // console.log(this.addedFilters)
+            console.log('appendfilter', this.appendFilter);
         }
     }
 
